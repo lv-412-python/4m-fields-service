82415b72b93a0793c66979a1c29e507ddce60584
Olha <8unicorn89@gmail.com>
2019-07-09 11:54:52 +0300

    reduced returns

--- fields_service/views/fields_views.py
+++ fields_service/views/fields_views.py
@@ -1,5 +1,5 @@
 """Creates resources."""
-from flask import request, Response
+from flask import request, Response, jsonify
 from flask_api import status
 from flask_restful import Resource
 from marshmallow import ValidationError
@@ -18,25 +18,26 @@ class FieldResource(Resource):
         :param field_id: int: id of requested field.
         :return json."""
         if not field_id:
-            data = []
             fields_id = request.json['fields']
             titles = {}
             for f_id in fields_id:
                 try:
                     field_title = Field.query.with_entities(Field.title).filter_by(id=f_id).first()
                 except DataError:
-                    data.append({"error": "Wrong input data"})
-                    data.append(status.HTTP_400_BAD_REQUEST)
+                    message = {"error": "Invalid url"}
+                    resp = jsonify(message)
+                    resp.status_code = status.HTTP_400_BAD_REQUEST
                     break
                 if not field_title:
-                    data.append({"error": "Does not exist."})
-                    data.append(status.HTTP_400_BAD_REQUEST)
+                    message = {"error": "Does not exist."}
+                    resp = jsonify(message)
+                    resp.status_code = status.HTTP_400_BAD_REQUEST
                     break
                 titles[f_id] = field_title.title
             else:
-                data.append(titles)
-                data.append(status.HTTP_200_OK)
-            return data[0], data[1]
+                resp = jsonify(titles)
+                resp.status_code = status.HTTP_200_OK
+            return resp
 
         try:
             field = Field.query.get(field_id)
@@ -114,7 +115,7 @@ class FieldResource(Resource):
             for choice in choices:
                 choice = Choice(title=choice['title'], field_id=field.id)
                 DB.session.add(choice)
-        try: 
+        try:
             DB.session.commit()
         except IntegrityError:
             DB.session.remove()
