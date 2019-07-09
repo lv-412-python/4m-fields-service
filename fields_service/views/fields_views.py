9a50ba352d367b58e344e1cd2a968c8091954436
Olha <8unicorn89@gmail.com>
2019-07-09 12:01:19 +0300

    changed resources

--- fields_service/views/fields_views.py
+++ fields_service/views/fields_views.py
@@ -1,5 +1,6 @@
 """Creates resources."""
-from flask import request, Response
+from flask import request, Response, jsonify
+from flask_api import status
 from flask_restful import Resource
 from marshmallow import ValidationError
 from sqlalchemy.exc import DataError, IntegrityError
@@ -10,30 +11,47 @@ from fields_service.models.field import Field
 from fields_service.serializers.field_schema import FieldSchema
 
 
-OK = 200
-BAD_REQUEST = 400
-NOT_FOUND = 404
-
-
 class FieldResource(Resource):
     """Resources class."""
-    def get(self, field_id):
+    def get(self, field_id=None):
         """Get route.
         :param field_id: int: id of requested field.
         :return json."""
+        if not field_id:
+            fields_id = request.json['fields']
+            titles = {}
+            for f_id in fields_id:
+                try:
+                    field_title = Field.query.with_entities(Field.title).filter_by(id=f_id).first()
+                except DataError:
+                    message = {"error": "Invalid url"}
+                    resp = jsonify(message)
+                    resp.status_code = status.HTTP_400_BAD_REQUEST
+                    break
+                if not field_title:
+                    message = {"error": "Does not exist."}
+                    resp = jsonify(message)
+                    resp.status_code = status.HTTP_400_BAD_REQUEST
+                    break
+                titles[f_id] = field_title.title
+            else:
+                resp = jsonify(titles)
+                resp.status_code = status.HTTP_200_OK
+            return resp
+
         try:
             field = Field.query.get(field_id)
         except DataError:
-            return {"error": "Invalid url."}, NOT_FOUND
+            return {"error": "Invalid url."}, status.HTTP_404_NOT_FOUND
         if field is None:
-            return {"error": "Does not exist."}, BAD_REQUEST
+            return {"error": "Does not exist."}, status.HTTP_400_BAD_REQUEST
         if field.has_choice:
             choices = Choice.query.filter_by(field_id=field.id).all()
             field.choices = choices
         field = FieldSchema().dump(obj=field).data
-        return field, OK
+        return field, status.HTTP_200_OK
 
-    def put(self, field_id):
+    def put(self, field_id=None):
         """put route.
         :param field_id: int: id of requested field.
         :return: int: status.
@@ -41,13 +59,13 @@ class FieldResource(Resource):
         try:
             field = Field.query.get(field_id)
         except DataError:
-            return {"error": "Invalid url."}, NOT_FOUND
+            return {"error": "Invalid url."}, status.HTTP_404_NOT_FOUND
         if not field:
-            return {"error": "Does not exist."}, BAD_REQUEST
+            return {"error": "Does not exist."}, status.HTTP_400_BAD_REQUEST
         try:
             data = FieldSchema().load(request.json).data
         except ValidationError as err:
-            return err.messages, BAD_REQUEST
+            return err.messages, status.HTTP_400_BAD_REQUEST
         choices = data.pop('choices', None)
         for key, value in data.items():
             setattr(field, key, value)
@@ -58,10 +76,10 @@ class FieldResource(Resource):
         try:
             DB.session.commit()
         except IntegrityError:
-            return {"Error": "Already exists."}, BAD_REQUEST
-        return Response(status=OK)
+            return {"Error": "Already exists."}, status.HTTP_400_BAD_REQUEST
+        return Response(status=status.HTTP_200_OK)
 
-    def delete(self, field_id):
+    def delete(self, field_id=None):
         """delete route.
         :param field_id: int: id of requested field.
         :return: int: status.
@@ -69,20 +87,16 @@ class FieldResource(Resource):
         try:
             field = Field.query.get(field_id)
         except DataError:
-            return {"error": "Invalid url."}, NOT_FOUND
+            return {"error": "Invalid url."}, status.HTTP_404_NOT_FOUND
         if field is None:
-            return {"error": "Does not exist."}, BAD_REQUEST
+            return {"error": "Does not exist."}, status.HTTP_400_BAD_REQUEST
         if field.has_choice:
             choices = Choice.query.filter_by(field_id=field.id).all()
             for choice in choices:
                 DB.session.delete(choice)
         DB.session.delete(field)
         DB.session.commit()
-        return Response(status=OK)
-
-
-class PostResource(Resource):
-    """Resources class."""
+        return Response(status=status.HTTP_200_OK)
 
     def post(self):
         """post route.
@@ -91,7 +105,7 @@ class PostResource(Resource):
         try:
             data = FieldSchema().load(request.json).data
         except ValidationError as err:
-            return err.messages, BAD_REQUEST
+            return err.messages, status.HTTP_400_BAD_REQUEST
         choices = data.pop('choices', None)
         field = Field(**data)
         DB.session.add(field)
@@ -108,17 +122,5 @@ class PostResource(Resource):
             DB.session.delete(field)
             DB.session.commit()
             DB.session.rollback()
-            return {"error": "Already exists."}, BAD_REQUEST
-        return Response(status=OK)
-
-    def get(self):
-        """Get route."""
-        fields_id = request.json['fields']
-        titles = {}
-        for f_id in fields_id:
-            try:
-                field_title = Field.query.with_entities(Field.title).filter_by(id=f_id).first()
-            except DataError:
-                return {"message": "Wrong input data"}, BAD_REQUEST
-            titles[f_id] = field_title.title
-        return titles
+            return {"error": "Already exists."}, status.HTTP_400_BAD_REQUEST
+        return Response(status=status.HTTP_200_OK)
