5da0b8a7c524865c5166a4ca09d75b876a3e931b
Olha <8unicorn89@gmail.com>
2019-08-06 14:02:34 +0300

    added owner

--- fields_service/models/field.py
+++ fields_service/models/field.py
@@ -7,6 +7,7 @@ from fields_service.db import DB
 class Field(DB.Model):
     """Class used to represent Field model."""
     id = Column(Integer, primary_key=True)
+    owner = Column(Integer())
     title = Column(String(100), nullable=False)
     has_choice = Column(Boolean, default=False)
     is_multichoice = Column(Boolean, default=False)
